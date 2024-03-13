f4785a1fb42416cd00dedc331f94a78297238766
Pe <152407859+ChristianPe2124@users.noreply.github.com>
2024-03-13 12:24:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let fanSpeed = document.getElementById("img");
+
+function funcOn() {
+  fanSpeed.style.animationDuration = 3 + "s";
+}
+function funcOff() {
+  fanSpeed.style.animationDuration = 0 + "s";
+}
+function fanspeed2() {
+  fanSpeed.style.animationDuration = 1 + "s";
+}
+function fanspeed3() {
+  fanSpeed.style.animationDuration = 0.5 + "s";
+}
+function fanspeed4() {
+  fanSpeed.style.animationDuration = 0.1 + "s";
+}
